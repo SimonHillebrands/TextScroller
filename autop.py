f66318748efc76b7384c3b185bc62ecffc6315ca
SimonHillebrands <55998350+SimonHillebrands@users.noreply.github.com>
2021-04-07 16:50:07 -0400

    Add files via upload

--- autop.py
+++ autop.py
@@ -5,15 +5,18 @@ import os
 import inspirobot
 
 try:
-	if(sys.argv[1] == "I"):
-		text = ""
-		flow = inspirobot.flow()
-		for quote in flow:
-			text = text + quote.text
-		print(text)
-		command = 'python test.py ' '"' + text + ' "'
+	if(len(sys.argv)>1):
+		if(sys.argv[1] == "I"):
+			text = ""
+			flow = inspirobot.flow()
+			for quote in flow:
+				text = text + quote.text
+			print(text)
+			command = 'python test.py ' '"' + text + ' "'
+		else:
+			command = 'python test.py ' + '"' + sys.argv[1] + ' "'
 	else:
-		command = 'python test.py ' + '"' + sys.argv[1] + ' "'
+		command = 'python test.py'
 except IndexError:
 	print("Your forgot to add an argument")
 os.system(command)
